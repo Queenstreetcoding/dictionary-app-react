4416e2740b946d22fee0613b138c025f68807f64
Cindy <108219366+Queenstreetcoding@users.noreply.github.com>
2023-07-06 14:52:26 +0200

    Bootstrap is installed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <a href="/" className="btn btn-primary">Hello</a>
       </header>
     </div>
   );
