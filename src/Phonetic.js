7f85dc19a124adcd50a7625438b8b81b9f65dfb6
Verboog <c.verboog@fugro.com>
2023-07-22 13:17:09 +0200

    Added react-audio-player

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { PlayCircle } from "react-bootstrap-icons";
 import "./Phonetic.css";
+import ReactAudioPlayer from 'react-audio-player';
 
 export default function Phonetic(props){
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-                <span className="Play"><PlayCircle size={40}/></span>
-            </a>
             <span className="Text"> {props.phonetic.text}</span>
+            <ReactAudioPlayer
+                className="mt-3"
+                src={props.phonetic.audio}
+                controls
+            />
         </div>
     )
 }
